b2f26ef5295a83bc07f965d9e0f0ee3d656a535a
David Babatunde <davidbabatundehbc@gmail.com>
2025-12-23 22:08:53 +0100

    Add question array and next question functionality

--- quiz-app/src/components/QuestionCard.jsx
+++ quiz-app/src/components/QuestionCard.jsx
@@ -1,16 +1,40 @@
 import { useState } from "react";
 
-function QuestionCard() {    
+function QuestionCard() { 
+    const questions = [
+        {
+            question: "Whatt is 2 + 2?",
+            options: [3, 4, 5, 6]
+        },
+        {
+            question: "Whatt is 22 - 16?",
+            options: [5, 6, 7, 8]
+        }
+    ]
+    const [CurrentIndex, SetCurrentIndex] = useState(0);
     const [SelectedAnswer, SetSelectedAnswer] = useState(null);
+    const currentQuestion = questions[CurrentIndex];
+
+
+    function NextQuestion() {
+        SetSelectedAnswer(null);
+        SetCurrentIndex(CurrentIndex + 1);
+    }
+
     return(
         <div>
-            <p>What is 2 + 2?</p>
-            <button onClick={() => SetSelectedAnswer("3")}>3</button>
-            <button onClick={() => SetSelectedAnswer("4")}>4</button>
-            <button onClick={() => SetSelectedAnswer("5")}>5</button>
-            <button onClick={() => SetSelectedAnswer("6")}>6</button>
+            <p>{currentQuestion.question}</p>
+
+            {currentQuestion.options.map((option) => (
+                <button key={option} onClick={() => SetSelectedAnswer(option)}>{option}</button>
+            ))}
 
             {SelectedAnswer && <p>You selected {SelectedAnswer}</p>}
+
+            <br />
+            {CurrentIndex < questions.length - 1 && (
+                <button onClick={NextQuestion}>Next</button>
+            )}
         </div>
     )
 }
